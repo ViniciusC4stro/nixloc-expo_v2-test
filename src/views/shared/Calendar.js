9cedad0482a2d6da61dab49746d3d28533a451d5
ViniciusC4stro <vinicius.c.d3v@Gmail.com>
2023-02-14 18:07:32 -0300

    Teste com calendário

--- src/views/shared/Calendar.js
+++ src/views/shared/Calendar.js
@@ -1,10 +1,44 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Text, StyleSheet, View, Alert } from "react-native";
+
+import { Calendar, Agenda } from "react-native-calendars";
 
 export default () => {
-    return(
-        <View>
-            <Text>Calendar</Text>
-        </View>
-    )
-}
+    const dayPress = () => {
+        Alert.alert('Esse dia foi pressionado!')
+    }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Calendário</Text>
+      <Calendar style={styles.calendar}
+            markingType={'custom'}
+            markedDates={{
+                '2023-02-18': {customStyles: {container:{backgroundColor:'black', elevation: 2}, text:{color:'white'}}},
+                '2023-02-28': {customStyles: {container:{backgroundColor:'blue', elevation: 2}, text:{color:'white'}}}
+            }}
+            onDayPress={() => dayPress()}
+            />
+      {/* <Agenda style={styles.agenda}
+      /> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    marginTop: 50,
+  },
+  calendar: {
+    marginTop: 50,
+  },
+  agenda: {
+    marginTop: 50,
+    width: "100%",
+    height: '100%',
+  }
+});
